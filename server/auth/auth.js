91db1656b38deea3df6bca82d9ded565b8c922b8
kornkritpawit <konsoo@hotmail.com>
2021-03-21 17:13:27 +0700

    fin

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -37,6 +37,25 @@ exports.getFreshUser = function() {
     // update req.user with fresh user from the
     // stale token data
 
+    User.findById(req.user._id)
+      .then(function(user) {
+        if (!user) {
+          // if no user is found it was not
+          // it was a valid JWT but didn't decode
+          // to a real user in our DB. Either the user was deleted
+          // since the client got the JWT, or
+          // it was a JWT from some other source
+          res.status(401).send('Unauthorized');
+        } else {
+          // update req.user with fresh user from
+          // stale token data
+          req.user = user;
+          next();
+        } 
+      }, function(err) {
+        next(err);
+      });
+
   }
 };
 
@@ -46,13 +65,38 @@ exports.verifyUser = function() {
     var password = req.body.password;
 
     // if no username or password then stop.
-
+    if (!username || !password) {
+      res.status(400).send('You need a username and password');
+      return;
+    }
     // look user up in the DB so we can check
     // if the passwords match for the username
 
     // use the authenticate() method on a user doc. Passin
     // in the posted password, it will hash the
     // password the same way as the current passwords got hashed
+
+    User.findOne({username: username})
+      .then(function(user) {
+        console.log(user)
+        if (!user) {
+          res.status(401).send('No user with the given username');
+        } else {
+          // checking the passowords here
+          if (!user.authenticate(password)) {
+            res.status(401).send('Wrong password');
+          } else {
+            // if everything is good,
+            // then attach to req.user
+            // and call next so the controller
+            // can sign a token from the req.user._id
+            req.user = user;
+            next();
+          }
+        }
+      }, function(err) {
+        next(err);
+      });
 
 
   };
